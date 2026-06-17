ad748ac8643be44ab80fdb7a4f86228e3508fe6c
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2026-06-17 11:44:15 -0700

    allow configure if console should be displayed in win32

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
+#include <clocale>
+
 #include "LawnApp.h"
 #include "Sexy.TodLib/TodStringFile.h"
-
-using namespace Sexy;
 
 bool (*gAppCloseRequest)();
 bool (*gAppHasUsedCheatKeys)();
 SexyString (*gGetCurrentLevelName)();
 
-int main(int argc, char **argv)
+int LawnMain()
 {
+	// make locale UTF-8 so console output displays correctly
+	std::setlocale(LC_ALL, "en_us.UTF-8");
+
 	TodStringListSetColors(gLawnStringFormats, gLawnStringFormatCount);
 	gGetCurrentLevelName = LawnGetCurrentLevelName;
 	gAppCloseRequest = LawnGetCloseRequest;
@@ -22,8 +25,30 @@ int main(int argc, char **argv)
 	gLawnApp->Start();
 
 	gLawnApp->Shutdown();
-	if (gLawnApp)
-		delete gLawnApp;
+	delete gLawnApp;
 
 	return 0;
+}
+
+// Windows subsystem entrypoint (doesn't show console when double-clicked)
+#ifdef WIN32
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// Attach to parent console when launched from a terminal
+	if (AttachConsole(ATTACH_PARENT_PROCESS))
+	{
+		FILE *dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		freopen_s(&dummy, "CONOUT$", "w", stderr);
+		SetConsoleCP(CP_UTF8);
+		SetConsoleOutputCP(CP_UTF8);
+	}
+	return LawnMain();
+}
+#endif
+
+// Console/GNU/UNIX entrypoint
+int main(int argc, char **argv)
+{
+	return LawnMain();
 };
